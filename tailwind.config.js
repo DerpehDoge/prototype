425b278007c462fe89d8bf0d62e22409b585efce
goose <rglyxr@gmail.com>
2021-04-12 15:34:16 -0500

    washing machine

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
         enabled: false,
         content: ["./pages/**/*.js", "./components/**/*.js"],
     },
-    darkMode: "class", // or 'media' or 'class'
+    darkMode: "media", // or 'media' or 'class'
     theme: {
         extend: {},
     },
